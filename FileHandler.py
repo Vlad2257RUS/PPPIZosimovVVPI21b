f248723563d127cc5dc4be7b19232607245f6300
Vlad2257RUS <124775101+Vlad2257RUS@users.noreply.github.com>
2025-02-13 13:16:01 +0300

    Delete FileHandler.py

--- FileHandler.py
+++
@@ -1,9 +0,0 @@
-class FileHandler:
-    def open_file(self, file_path):
-        with open(file_path, 'r') as file:
-            return file.read()
-
-    def save_file(self, file_path, content):
-        with open(file_path, 'w') as file:
-            file.write(content)
-
