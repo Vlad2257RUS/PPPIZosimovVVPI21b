4ef31334d482f6690e19cadb4b7128d26a13dcfe
Vlad2257RUS <124775101+Vlad2257RUS@users.noreply.github.com>
2025-02-16 14:02:24 +0300

    Update theme_support.py

--- theme_support.py
+++ theme_support.py
@@ -1,2 +1,10 @@
-def change_theme(theme):
-    print(f"Смена темы на: {theme}")
+class UserInterface:
+    def __init__(self):
+        print("Инициализация интерфейса пользователя...")
+
+    def create_interface(self):
+        print("Создание интерфейса пользователя...")
+        print("Добавление новых функций...")
+
+    def change_theme(self, theme):
+        print(f"Смена темы на: {theme}")
