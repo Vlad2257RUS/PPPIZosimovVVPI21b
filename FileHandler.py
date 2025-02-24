4fc18d5f2d3af0a2d8d764964df14299345971e5
Vlad2257RUS <124775101+Vlad2257RUS@users.noreply.github.com>
2025-02-24 10:40:09 +0300

    Update FileHandler.py Добавление  DocBook-комментариев

--- FileHandler.py
+++ FileHandler.py
@@ -1,9 +1,21 @@
 class FileHandler:
+    """
+    Класс для работы с файлами.
+
+    Этот класс предоставляет функции для открытия и сохранения файлов.
+
+    Методы:
+        open_file(file_path): Открытие файла и чтение его содержимого.
+        save_file(file_path, content): Сохранение содержимого в файл.
+    """
+
     def open_file(self, file_path):
+        """Открытие файла и чтение его содержимого."""
         with open(file_path, 'r') as file:
             return file.read()
 
     def save_file(self, file_path, content):
+        """Сохранение содержимого в файл."""
         with open(file_path, 'w') as file:
             file.write(content)
 
